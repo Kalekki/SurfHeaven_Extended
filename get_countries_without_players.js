e6e01385f17fd5cc61c0f809ed743ac940cb9eb1
Kalekki <kalekkiorg@gmail.com>
2023-03-01 21:35:36 +0200

    helpers

---
+++ get_countries_without_players.js
@@ -0,0 +1,37 @@
+//useful so we dont populate ctop with empty countries
+//ms to wait between requests
+let cooldown = 400;
+
+async function fetchData(countryCodes) {
+  const emptyResponseCountries = [];
+  for (const countryCode of countryCodes) {
+    console.log("fetching ",countryCode)
+    await new Promise(resolve => setTimeout(resolve, cooldown));
+    const response = await fetch(`https://surfheaven.eu/api/ctop/${countryCode}`);
+    if (!response.ok) {
+      throw new Error(`Request for country code ${countryCode} failed with status ${response.status}`);
+    }
+    const data = await response.json();
+    if (!Object.keys(data).length) {
+      emptyResponseCountries.push(countryCode);
+    }
+  }
+  return emptyResponseCountries;
+}
+// countries that have players have been filtered from this list
+countries = [
+    "AFG","ASM","ATA","BHS","BRB","BEN","BTN","BES","BWA","BVT","VGB","IOT","BFA","BDI","CMR","CAF","TCD","CXR","CCK","COM","COG","COD","COK",
+    "CUB","DJI","DMA","GNQ","ERI","ETH","FLK","FJI","GUF","PYF","ATF","GAB","GMB","GRD","GIN","GNB","GUY","HTI","HMD","VAT","JAM","KIR","LAO",
+    "LSO","LBR","MWI","MHL","MYT","FSM","MSR","MOZ","NRU","NCL","NIC","NER","NIU","NFK","PLW","PNG","PCN","RWA","BLM","SHN","KNA","LCA","MAF",
+    "VCT","WSM","STP","SLE","SXM","SLB","SOM","SGS","SSD","SUR","SJM","SWZ","TLS","TGO","TKL","TON","TKM","TCA","TUV","UGA","UMI","VUT","VIR",
+    "WLF","ESH","YEM","ZWE"
+    ] // put those suckers in here
+
+fetchData(countries)
+  .then(emptyResponseCountries => {
+    console.log('Countries with empty response:', emptyResponseCountries);
+  })
+  .catch(error => {
+    console.error(error);
+  });
+
---
+++ get_missing_map_pics.js
@@ -0,0 +1,47 @@
+// gets all the maps that are on surfheaven but don't have a picture on the github repo
+// run this code in a browser console to print out the missing pics array
+let pics_url = "https://api.github.com/repos/Sayt123/SurfMapPics/git/trees/Maps-and-bonuses?recursive=1"
+let maps_url = "https://surfheaven.eu/api/maps"
+
+function get_missing_map_pics() {
+    let pics = JSON.parse(httpGet(pics_url));
+    let map_pics = [];
+    for (let i = 0; i < pics.tree.length; i++) {
+        let pic = pics.tree[i];
+        if (pic.path.includes("surf_")) {
+            pic.path = pic.path.replace("csgo/", "");
+            pic.path = pic.path.replace(".jpg", "");
+            map_pics.push(pic.path);
+        }
+    }
+    return map_pics;
+}
+
+function get_sh_maps() {
+    let sh_maps = JSON.parse(httpGet(maps_url));
+    let maps = [];
+    for (let i = 0; i < sh_maps.length; i++) {
+        let map = sh_maps[i];
+        maps.push(map.map);
+    }
+    return maps;
+
+}
+
+function httpGet(theUrl) {
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.open("GET", theUrl, false); 
+    xmlHttp.send(null);
+    return xmlHttp.responseText;
+}
+
+let maps_with_pics = get_missing_map_pics();
+let sh_maps = get_sh_maps();
+let missing_pics = [];
+for (let i = 0; i < sh_maps.length; i++) {
+    let map = sh_maps[i];
+    if (!maps_with_pics.includes(map)) {
+        missing_pics.push(map);
+    }
+}
+console.log(missing_pics);
